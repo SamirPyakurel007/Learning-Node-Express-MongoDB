9a2ee61fc26a3e9b8c5feec5fedffce2b421dc68
samir <samirpyakurel07@gmail.com>
2026-01-21 15:03:04 +0545

    feat: create tour schema and save a document

---
+++ 05practice/server.js
@@ -0,0 +1,56 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = require("./app");
+
+dotenv.config({ path: "./.env" });
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("db connection successful");
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("error: ", err);
+  });
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
