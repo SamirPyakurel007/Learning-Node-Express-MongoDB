d02b3faaa428839c0ece4a3492105f5e56b4ff27
samir <samirpyakurel07@gmail.com>
2026-01-23 01:31:21 +0545

    feat: add custom errors for user misinput

--- 05practice/controllers/tourControllers.js
+++ 05practice/controllers/tourControllers.js
@@ -1,3 +1,4 @@
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
@@ -28,11 +29,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
   if (!tour) {
-    res.status(404).json({
-      status: "failed",
-      message: "Tour not found",
-    });
+    return next(new AppError("Tour not found", 404));
   }
 
   res.status(200).json({
@@ -61,6 +60,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError("Tour not found", 404));
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -70,7 +73,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("Tour not found", 404));
+  }
+
   res.status(204).json({
     status: "success",
   });
