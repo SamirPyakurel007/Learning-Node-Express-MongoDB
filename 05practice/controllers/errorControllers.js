e9934739ea1b9096bc566287f6e9b21f8875d37b
samir <samirpyakurel07@gmail.com>
2026-01-23 11:39:36 +0545

    feat: error handling for validation, cast error, duplicate key

--- 05practice/controllers/errorControllers.js
+++ 05practice/controllers/errorControllers.js
@@ -1,9 +1,39 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = (err) => {
+  const message = `Duplicate Field value: "${err.keyValue.name}". please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
+
+  let error = { ...err };
+  if (error.name === "CastError") {
+    error = handleCastErrorDB(error);
+  }
+  if (error.code === 11000) {
+    error = handleDuplicateErrorDB(error);
+  }
+  if (error.name === "ValidationError") {
+    error = handleValidationErrorDB(error);
+  }
+
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
   });
 };
 
